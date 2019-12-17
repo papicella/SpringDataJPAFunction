5ef19096275c3c81ed32e6d2a84d0ce5272612eb
papicella <papicella@pivotal.io>
2019-12-17 12:18:56 +1100

    Added Function bean

--- src/main/java/com/example/demo/SpringDataJpaFunctionApplication.java
+++ src/main/java/com/example/demo/SpringDataJpaFunctionApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.entity.Person;
 import com.example.demo.entity.PersonRepository;
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,17 +19,17 @@ public class SpringDataJpaFunctionApplication {
         this.personRepository = personRepository;
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringDataJpaFunctionApplication.class, args);
-    }
-
     @Bean
     public Function<Long, String> findPersonById() {
         return id -> {
+            Gson gson = new Gson();
             Optional<Person> person = personRepository.findById(id);
-            Person p = person.get();
-            return p.toString();
+            return person.isPresent() ? gson.toJson(person.get()) : null;
         };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaFunctionApplication.class, args);
     }
 
 }
