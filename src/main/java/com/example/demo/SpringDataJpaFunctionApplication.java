d0286b6196b0b80e9848a2162677ac86b7f25d4b
papicella <papicella@pivotal.io>
2019-12-17 15:35:21 +1100

    Added Function bean for findByLastName

--- src/main/java/com/example/demo/SpringDataJpaFunctionApplication.java
+++ src/main/java/com/example/demo/SpringDataJpaFunctionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -25,6 +26,15 @@ public class SpringDataJpaFunctionApplication {
             Gson gson = new Gson();
             Optional<Person> person = personRepository.findById(id);
             return gson.toJson(person.orElse(null));
+        };
+    }
+
+    @Bean
+    public Function<String, String> findByLastName() {
+        return surname -> {
+            Gson gson = new Gson();
+            List<Person> personList = personRepository.findByLastName(surname.toLowerCase());
+            return gson.toJson(personList);
         };
     }
 
